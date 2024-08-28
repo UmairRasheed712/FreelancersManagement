2e901828ceabfbadc549972bcb3ac553ce6cd029
Shaheen107 <mrizwantariq107@gmail.com>
2024-08-28 19:35:44 +0500

    Initial Commit

---
+++ FreelancersManagement/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FreelancersManagement
+//
+//  Created by Apple on 28/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
